2b79f061e8b81f2fe9bb0222c89a195001c584f9
thiagogagliari <thiagogagliari@hotmail.com>
2022-07-25 22:44:59 -0300

    c-thiago-gagliari

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,13 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(`Minha string antes tinha ${minhaString.length} caracteres.`)
+
+//a
+const novaString = minhaString.trim()
+
+//b
+console.log(`Minha string depois tem ${novaString.length} caracteres.`)
+
+//c
+console.log(novaString.replace("________","sticioso"))
+
