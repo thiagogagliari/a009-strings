3f3fa61f40c623df45ee9d2618b781941a50fa10
thiagogagliari <thiagogagliari@hotmail.com>
2022-07-25 22:52:50 -0300

    barbosa-thiago-gagliari

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const nome = prompt('Qual seu nome?')
+const comida1 = prompt('Qual sua primeira comida favorita?')
+const comida2 = prompt('Qual sua segunda comida favorita?')
+const comida3 = prompt('Qual sua terceira comida favorita?')
+
+console.log(`Estas são as comidas favoritas do ${nome} \n- ${comida1} \n- ${comida2} \n- ${comida3}`)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,13 @@
-//Crie a const para a frase aqui
+//Frase: Jorge tem uma casa rosa com portão laranja, com os dizeres: "BOAS VINDAS", mas não deixe o gato sair.
+
+//b
+const colorHouse = "rosa"
+const colorGate = "laranja"
+
+//Exercício Extra
+const cat = "mas não deixe o gato sair."
+
+//a
+const frase = `Jorge tem uma casa ${colorHouse} e com portão ${colorGate}, com os dizeres: "BOAS VINDAS", ${cat.toUpperCase()}`
+
+console.log(frase);
